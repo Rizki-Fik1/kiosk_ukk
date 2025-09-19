7f22325984d5016e89690d776e5cdc142ddc5a46
pikakso <fikoaja9@gmail.com>
2025-09-19 07:52:19 +0700

    make create products

--- database/migrations/2025_09_15_062525_create_products_table.php
+++ database/migrations/2025_09_15_062525_create_products_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrationns.
      */
     public function up(): void
     {
