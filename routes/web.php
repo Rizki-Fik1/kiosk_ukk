cb0c95d5a671594de5ac4d1d253a456fd34b6eaf
pikakso <fikoaja9@gmail.com>
2025-10-05 11:59:06 +0700

    Solve Route Error

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PurchaseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,18 +13,26 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'role:admin'])->group(function () {
     // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Products
+    // Products (existing pattern)
     Route::get('/admin/products', [ProductController::class, 'index'])->name('products.index');
     Route::post('/admin/products', [ProductController::class, 'store'])->name('products.store');
     Route::put('/admin/products/{product}', [ProductController::class, 'update'])->name('products.update');
     Route::delete('/admin/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
 
+    // Purchases (same pattern as Products)
+    Route::get('/admin/purchases', [PurchaseController::class, 'index'])->name('purchases.index');
+    Route::get('/admin/purchases/create', [PurchaseController::class, 'create'])->name('purchases.create');
+    Route::post('/admin/purchases', [PurchaseController::class, 'store'])->name('purchases.store');
+    Route::get('/admin/purchases/{purchase}', [PurchaseController::class, 'show'])->name('purchases.show');
+    Route::get('/admin/purchases/{purchase}/edit', [PurchaseController::class, 'edit'])->name('purchases.edit');
+    Route::put('/admin/purchases/{purchase}', [PurchaseController::class, 'update'])->name('purchases.update');
+    Route::delete('/admin/purchases/{purchase}', [PurchaseController::class, 'destroy'])->name('purchases.destroy');
 });
 
 
